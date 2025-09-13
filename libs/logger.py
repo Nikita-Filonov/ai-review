90ab62d9d20c9cfde857baea9735720ba3027621
Nikita Filonov <nikita.filonov@dif.tech>
2025-09-13 14:24:26 +0300

    libs logger

--- libs/logger.py
+++ libs/logger.py
@@ -3,7 +3,7 @@ import logging
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     if not logger.hasHandlers():
         handler = logging.StreamHandler()
