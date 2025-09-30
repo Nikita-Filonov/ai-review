d090a7948a359fba73cba43d5db5b27d80e0cfce
Nikita Filonov <nikita.filonov@dif.tech>
2025-09-30 22:05:44 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,11 @@
 pytest_plugins = (
-    "ai_review.tests.fixtures.git"
+    "ai_review.tests.fixtures.git",
+    "ai_review.tests.fixtures.llm",
+    "ai_review.tests.fixtures.vcs",
+    "ai_review.tests.fixtures.diff",
+    "ai_review.tests.fixtures.cost",
+    "ai_review.tests.fixtures.prompt",
+    "ai_review.tests.fixtures.artifacts",
+    "ai_review.tests.fixtures.review.inline",
+    "ai_review.tests.fixtures.review.summary"
 )
